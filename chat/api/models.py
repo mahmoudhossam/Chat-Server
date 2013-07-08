a9ff4b4558ebe93cefc7b76ae3f313d3028c0c43
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2013-07-08 17:58:00 +0200

    Fix clashing ForeignKeys

--- chat/api/models.py
+++ chat/api/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 class Message(models.Model):
     """Represents a message received by the server"""
     text = models.CharField(max_length=10)
-    sent_by = models.ForeignKey(User)
-    sent_to = models.ForeignKey(User)
+    sender = models.ForeignKey(User, related_name='message_sender')
+    recipient = models.ForeignKey(User, related_name='message_recipient')
     sent_at = models.DateTimeField(auto_now_add=True)
