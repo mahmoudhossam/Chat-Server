1b3313aba893313af2e35e6228a5b24044ec2ba8
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2013-07-15 15:55:36 +0200

    Add messages manager

--- chat/api/models.py
+++ chat/api/models.py
@@ -2,8 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Messages(models.Manager):
+
+    def sent_by(self, user):
+        return self.objects.filter(sender=user)
+
+
 class Message(models.Model):
     """Represents a message received by the server"""
     text = models.CharField(max_length=10)
     sender = models.ForeignKey(User, related_name='message_sender')
     recipient = models.ForeignKey(User, related_name='message_recipient')
+
+    objects = models.Manager()
+    messages = Messages()
