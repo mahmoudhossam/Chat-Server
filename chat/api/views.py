8cec5462a663a59163ed787144502198d28a8183
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2013-07-21 01:55:48 +0200

    Add API views

--- chat/api/views.py
+++ chat/api/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
+from rest_framework import status
+from .models import Message
+from .serializers import UserSerializer, MessageSerializer
 
-# Create your views here.
+
+class UserView(APIView):
+
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MessageView(APIView):
+
+    def get(self, request, format=None):
+        messages = Message.objects.all()
+        serializer = MessageSerializer(messages, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = MessageSerializer(data=request.DATA)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
