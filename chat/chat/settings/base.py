cd91db3dbb35763c1ca349a9bb1e9f1de1f55aec
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2013-07-21 01:55:25 +0200

    Configure renderers

--- chat/chat/settings/base.py
+++ chat/chat/settings/base.py
@@ -57,3 +57,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
+}
