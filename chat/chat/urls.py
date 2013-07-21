579bc4cc9a369916c9f433243f4443fd0fc52dc4
Mahmoud Hossam <mahmoud.h.hanafy@gmail.com>
2013-07-21 02:18:26 +0200

    Add API urls

--- chat/chat/urls.py
+++ chat/chat/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
+from api import views
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'chat.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^users/$', views.UserView.as_view()),
+    url(r'^messages/$', views.MessageView.as_view()),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
